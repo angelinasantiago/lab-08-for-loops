e8e49bc90ce10d0aa96f0a8035003cef96cc9bf0
ANGELINA <ang@MacBook-Air.home>
2021-04-05 20:39:52 -0400

    finished fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,22 @@
+for i in range (1, 101, 1):
+    if (i % 3 == 0 and i % 5 == 0):
+        print("FizzBuzz")
+    elif (i % 3 == 0):
+        print("Fizz")
+    elif (i % 5 == 0):
+        print("Buzz")
+    else:
+        print(i)
+
+
+# for (var i = 1; i <=100; i++) {
+# if (i % 3 == 0 && i % 5 == 0) {
+#console.log("FizzBuzz");
+#}  else if (i % 3 == 0) {
+#console.log("Fizz");
+#} else if (i % 5 == 0) {
+#console.log("Buzz");
+#} else {
+#console.log(i);
+#}
+#}
